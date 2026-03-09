e679944ab99b07d19aa57cfc3f5a2a370a371b72
Victor Makokha <marcos@Victors-MacBook-Pro.local>
2026-03-09 08:46:44 +0300

    fix parse cv issue in production build

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,4 +1,5 @@
 export async function register() {
+    console.log('[Instrumentation] Registering workers...', process.env.NEXT_RUNTIME);
    if (process.env.NEXT_RUNTIME === "nodejs") {
     const { startWorkers } = await import("@/lib/queue/start-workers");
     startWorkers();
